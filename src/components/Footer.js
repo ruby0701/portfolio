446331b63f90902a455b3cecca9eb338a10804ff
ruby0701 <ruby0701@gmail.com>
2023-03-30 23:05:42 +0800

    added code to the current 4 component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 function Footer() {
     return (
-        <footer>Ruby Wang 2023</footer>
+        <div>
+            <footer>Ruby Wang 2023</footer>
+
+            <div>
+                <a href="mailto:ruby0701@gmail.com">ruby0701@gmail.com</a>
+                <div >
+                    <a href="https://www.linkedin.com/feed/" target='_blank' rel="noreferrer">
+                        <LinkedInIcon></LinkedInIcon>
+                    </a>
+
+                    <a href="https://github.com/ruby0701" target='_blank' rel="noreferrer">
+                        <GitHubIcon></GitHubIcon>
+                    </a>
+                </div>
+            </div>
+        </div>
     );
 }
 export default Footer;
--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,8 +1,71 @@
 import React from 'react';
+import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import SchoolIcon from '@mui/icons-material/School';
+import WorkIcon from '@mui/icons-material/Work';
 
 function Timeline() {
     return (
-        <h1>Timeline placeholder</h1>
+        <div className='timeline'>
+            <VerticalTimeline lineColor='black'>
+                <VerticalTimelineElement
+                    className='vertical-timeline-element--education'
+                    date="2013.09-2017.06"
+                    iconStyle={{ background: "black", color: "white" }}
+                    icon={<SchoolIcon></SchoolIcon>}
+                >
+                    <h3 className='vertical-timeline-element-title'>
+                        National Taipei University, New Taipei City
+                    </h3>
+                    <p>Bachelor Degree</p>
+                </VerticalTimelineElement>
+                <VerticalTimelineElement
+                    className='vertical-timeline-element--education'
+                    date="2017.09-2018.04"
+                    iconStyle={{ background: "black", color: "white" }}
+                    icon={<SchoolIcon></SchoolIcon>}
+                >
+                    <h3 className='vertical-timeline-element-title'>
+                        National Normal University, Taipei City
+                    </h3>
+                    <p>Master Degree (Unfinshed)</p>
+                </VerticalTimelineElement>
+                <VerticalTimelineElement
+                    className='vertical-timeline-element--education'
+                    date="2019.04-2020.08"
+                    iconStyle={{ background: "black", color: "white" }}
+                    icon={<SchoolIcon></SchoolIcon>}
+                >
+                    <h3 className='vertical-timeline-element-title'>
+                        Aso College, Hakata, Japan
+                    </h3>
+                    <p>Bachelor Degree (Unfinshed)</p>
+                </VerticalTimelineElement>
+
+                <VerticalTimelineElement
+                    className='vertical-timeline-element--education'
+                    date="2020.12-2021.08"
+                    iconStyle={{ background: "black", color: "white" }}
+                    icon={<WorkIcon></WorkIcon>}
+                >
+                    <h3 className='vertical-timeline-element-title'>
+                        National Cheng Kung University,EE Department, Tainan
+                    </h3>
+                    <p>Research Assitant</p>
+                </VerticalTimelineElement>
+                <VerticalTimelineElement
+                    className='vertical-timeline-element--education'
+                    date="2021.09-present"
+                    iconStyle={{ background: "black", color: "white" }}
+                    icon={<WorkIcon></WorkIcon>}
+                >
+                    <h3 className='vertical-timeline-element-title'>
+                        Taiwan Obayashi Group, Taipei
+                    </h3>
+                    <p>Coordinator</p>
+                </VerticalTimelineElement>
+            </VerticalTimeline>
+        </div >
     );
 }
 
